d43fa864e512ed8c11e3dab039285acf055be56c
marvinrencn <102325566@qq.com>
2021-01-11 15:23:29 +0800

    portal

---
+++ onegateway/src/main/java/cn/ren/marvin/giraffe/gateway/configueration/TokenGloabelFilter.java
@@ -0,0 +1,9 @@
+package cn.ren.marvin.giraffe.gateway.configueration; /**
+ * @author ：marvin ren
+ * @date ：Created in  2021-01-11 下午3:05
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+public class TokenGloabelFilter {
+}
